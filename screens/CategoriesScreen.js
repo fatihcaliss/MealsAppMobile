00386810dacb531972facc739d9730bf8abd6453
fatihcaliss <fthcls@gmail.com>
2023-11-22 16:34:12 +0300

    displaying item in a grid

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,12 +1,22 @@
-import { FlatList } from "react-native"
-import { CATEGORIES } from "../data/dummy-data"
+import { FlatList } from "react-native";
+import { CATEGORIES } from "../data/dummy-data";
+import CategoryGridTile from "../components/CategoryGridTile";
 
-function renderCategoryItem(item){
-    return
+function renderCategoryItem(itemData) {
+  return (
+    <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
+  );
 }
 
 function CategoriesScreen() {
-    return <FlatList data={CATEGORIES} keyExtractor={(item) => item.id} renderItem={renderCategoryItem} />
+  return (
+    <FlatList
+      data={CATEGORIES}
+      keyExtractor={(item) => item.id}
+      renderItem={renderCategoryItem}
+      numColumns={2}
+    />
+  );
 }
 
-export default CategoriesScreen
+export default CategoriesScreen;
