e1e1d3f03baf114624e8d411ea37c98e23c33014
fatihcaliss <fthcls@gmail.com>
2023-11-22 15:10:41 +0300

    dummy data added

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,12 @@
+import { FlatList } from "react-native"
+import { CATEGORIES } from "../data/dummy-data"
+
+function renderCategoryItem(item){
+    return
+}
+
+function CategoriesScreen() {
+    return <FlatList data={CATEGORIES} keyExtractor={(item) => item.id} renderItem={renderCategoryItem} />
+}
+
+export default CategoriesScreen
